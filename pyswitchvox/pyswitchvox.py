93708a36086df8c0c8c45d5315771e7aa5164612
Steve Castle <steve.castle@drssmail.com>
2015-10-08 14:17:14 -0700

    updated

--- pyswitchvox/pyswitchvox.py
+++ pyswitchvox/pyswitchvox.py
@@ -15,47 +15,33 @@ class SwitchVox:
         self.server = server
 
     def build_json(self, http_method, params):
-        """
-        Given a valid switchvox method and a dictionary of params
-        create a valid switchvox request json structure.
-        """
-        json_data = {'request': {'method': http_method,
-                                 'parameters': params
-                                 }
-                     }
+        json_data = {'request':{ 'method':http_method,
+                            'parameters': params
+                          }
+               }
         return json.dumps(json_data)
-
-    def extensions(self, extensions):
-        """
-        Given a list of extensions return detailed information on each extension.
-        """
+        
+    def get_info(self, extensions):
         http_method = 'switchvox.extensions.getInfo'
         params = {'extensions': extensions}
         json_data = self.build_json(http_method, params)
         r = requests.post(self.server,
-                          data=json_data,
-                          headers=self.headers,
-                          auth=HTTPDigestAuth(self.user, self.password),
-                          verify=False)
-        return r.json()['response']['result']
+                         data = json_data,
+                         headers=self.headers,
+                         auth=HTTPDigestAuth(self.user,self.password),
+                         verify=False)
+        print r.json()
+        return r
 
-    def call(self, from_phone_number, to_phone_number):
-        """
-        Initiate a call between a number and a second number, either internal
-        or external.
-        """
+    def call(self, to_phone_number, from_phone_number):
         http_method = 'switchvox.call'
-        account_id = self.extension(from_phone_number).response.account_id
-        params = {'dial_first': from_phone_number,
-                  'dial_second': to_phone_number,
-                  'dial_as_account_id': account_id
-                  }
+        params = {'dial_first': from_phone_number, 'dial_second': to_phone_number, 'dial_as_account_id': 1170}
         json_data = self.build_json(http_method, params)
         print(json_data)
         r = requests.post(self.server,
-                          data=json_data,
-                          headers=self.headers,
-                          auth=HTTPDigestAuth(self.user, self.password),
-                          verify=False)
+                         data = json_data,
+                         headers=self.headers,
+                         auth=HTTPDigestAuth(self.user,self.password),
+                         verify=False)
         print r.json()
         return r
